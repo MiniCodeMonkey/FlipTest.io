0352b170e1c8ac42c61e575b0e0f0004e992bd55
Mathias Hansen <me@codemonkey.io>
2013-06-08 15:37:22 -0400

    Screw this, let's use Vagrant

---
+++ app/config/dev/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'fliptest',
+			'username'  => 'fliptest',
+			'password'  => 'xGbFreXMijTQc3', /* Yes this password will be in the public GitHub repo - But it's a Hackathon, please don't judge! :) */
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+		
+	),
+
+);
